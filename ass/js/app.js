fc34d0096cb01819fb1e4da3a936ae58d8eb12f2
walter wick <83695885+walterwick@users.noreply.github.com>
2023-08-24 20:33:06 +0300

    Update app.js

--- ass/js/app.js
+++ ass/js/app.js
@@ -1,1 +1,37 @@
-1.js
+document.addEventListener("DOMContentLoaded", function() {
+
+  const modeToggle = document.querySelector('.mode-toggle');
+  const body = document.body;
+  
+  function enableNightMode() {
+      body.classList.add('night-mode');
+      modeToggle.querySelector('.fa-sun').style.display = 'inline';
+      modeToggle.querySelector('.fa-moon').style.display = 'none';
+  }
+  
+  function disableNightMode() {
+      body.classList.remove('night-mode');
+      modeToggle.querySelector('.fa-sun').style.display = 'none';
+      modeToggle.querySelector('.fa-moon').style.display = 'inline';
+  }
+  
+  modeToggle.addEventListener('click', () => {
+      body.classList.toggle('night-mode');
+      const isNightMode = body.classList.contains('night-mode');
+  
+      if (isNightMode) {
+          enableNightMode();
+      } else {
+          disableNightMode();
+      }
+  });
+  
+  // Check if dark mode is preferred
+  const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  console.log("Dark mode detected:", isDarkMode);
+  
+  // Apply dark mode by default if preferred
+  if (isDarkMode) {
+      enableNightMode();
+  }
+});
