7a42154a26749c8d5a076ecc96e847edf0e2c2d0
walter wick <83695885+walterwick@users.noreply.github.com>
2023-08-27 10:53:40 +0300

    Update 1.php

--- api/x/1.php
+++ api/x/1.php
@@ -1,5 +1,40 @@
 php 6.5
+
+
 <?php
+
+$goldRate = '';
+
+// URL'yi belirleyin
+$url = "https://www.google.com/finance/quote/USD-TRY";
+
+// cURL ile sayfayı çekin
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$response = curl_exec($ch);
+
+// XPath ile hedef elementi bulma
+$doc = new DOMDocument();
+@$doc->loadHTML($response);
+$xpath = new DOMXPath($doc);
+$xpath_expression = '//*[@id="yDmH0d"]/c-wiz[2]/div/div[4]/div/main/div[2]/c-wiz/div/div[1]/div/div[1]/div/div[1]/div/span/div/div';
+$target_element = $xpath->query($xpath_expression);
+
+// Eğer hedef element bulunursa içeriğini alabilirsiniz
+if ($target_element->length > 0) {
+    $goldRate = $target_element->item(0)->nodeValue;
+   // echo "ALTIN Kuru: " . $goldRate;
+} else {
+    echo "Veri çekilemedi.";
+}
+
+// cURL bağlantısını kapatın
+curl_close($ch);
+
+
+<p class="au"> ALTIN Kuru: <?php echo $goldRate; ?></p>
+
+
 phpinfo();
 ?>
     
